4b3ae3f3ad9fec3cbdba5bf0b8e3960d68860596
21 <48954080+Lilhxr@users.noreply.github.com>
2020-08-06 20:06:47 +0800

    Update negative_testcase.py

--- negative_testcase.py
+++ negative_testcase.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""module docstring"""
-
 import tensorflow as tf
 import numpy as np
 import unittest
 import os
 import sys
+
 this_dir = os.getcwd()
 sys.path.append(os.path.join(this_dir, '..'))
-
 from operator_testcase import OperatorTestCase
-class NegTestCase(OperatorTestCase): 
-    #调用OperatorTestCase类
+
+class NegTestCase(OperatorTestCase):
+#调用OperatorTestCase类
     def init_data(self):
-        #data1理解为输入张量，C，H，W三个维度
-        data1 = [
+        x1 = [
             [[-1, -1, -1],
              [4, 5, 6],
              [1, 1, 1]],
@@ -24,25 +22,28 @@ class NegTestCase(OperatorTestCase):
              [1, 1, 1],
              [7, 8, 9]],
         ]
-        x1 = np.array([data1])
-        self.x = [x1]
-        self.op_name = 'neg'
-        #将init_data对象转化为NegTestCase对象
+        x2 = [
+            [[-2, -2, -2],
+             [-2, -2, 2],
+             [-2, -2, 2]],
+
+            [[-1, -2, -3],
+             [4, 5, 6],
+             [1, 1, 1]]
+        ]
+        self.x = [np.array([x1]), np.array([x2])]
+        #self.op_name = 'Neg' 可加可不加
+        #将父类对象转换为子类
         super(self.__class__, self).init_data()
-
-     #定义一个神经网络
+        #定义网络
     def tf_net(self):
-        # 输入
         inputs = self.inputs()
-
-        # 算子
-        output = tf.negative(inputs[0], name=self.output_name)
+        #inputs可以读取多个数据输入 如果仅测试一个数据（比如x1）则改为input[0]
+        output = tf.negative(inputs,name=self.output_name)
         return output
-
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(NegTestCase("test"))
-
     runner = unittest.TextTestRunner()
     runner.run(suite)
